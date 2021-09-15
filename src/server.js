b4af6f6823b642349bf86469f81b8ca783cf6438
Alessandro L. Santos <aleluizsantos@gmail.com>
2021-09-15 08:58:52 -0300

    Change in port 3333 to 4000

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const errorHandler = require("./errors/handler");
 const app = express();
 
 const { NODE_INSTANCE } = process.env;
-const PORT = 3333 + parseInt(NODE_INSTANCE);
+const PORT = 4000 + parseInt(NODE_INSTANCE);
 
 //Fazer com que a aplicação ousa tanto o protocolo http quanto socket
 const server = require("http").Server(app);
@@ -33,7 +33,7 @@ app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 require("./app/controller/index")(app);
 
 server.listen(process.env.PORT || PORT, function () {
-  console.log("..::Servidor online::..");
+  console.log(`>> Servidor online in Port ${PORT}`);
 });
 
 let clients = [];
