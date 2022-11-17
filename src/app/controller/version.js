7d1b95ca9ec451ebc8835f18b36917c310c3d24a
Alessandro L. Santos <aleluizsantos@gmail.com>
2022-11-16 21:41:22 -0300

    atualização rota Version

--- src/app/controller/version.js
+++ src/app/controller/version.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const package = require("../../../package.json");
 
 router.get("/", (req, res) => {
   const organizationName = "lesoftware";
-  const version = "1.1.0";
+  const { version, description } = package;
+
   const email = "contato@lesoftware.com.br";
   const site = "www.lesoftware.com.br";
-  return res.json({ version, organizationName, email, site });
+  return res.json({ version, organizationName, description, email, site });
 });
 
 module.exports = (app) => app.use("/version", router);
